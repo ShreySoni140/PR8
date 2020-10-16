991052fd8363a0c925f563d22a9c76e0aa011468
Shrey Soni <180013107011@adit.ac.in>
2020-10-16 20:26:46 +0530

    Initial commit

---
+++ Suggestion.php
@@ -0,0 +1,74 @@
+<html >
+<head>
+<head><title>Suggestion</title></head>
+<style>
+h1
+{
+ -webkit-box-reflect:below -5px -webkit-gradient(linear, left top, left bottom, from(transparent), color-stop(16%,transparent) , to(rgba(250,250,250,.4)));
+   margin-top:-60px;
+   font-style:oblique;
+   margin-left:100px;
+   color:#FFFFFF;
+   font-size:50px;
+}
+
+nav {
+	margin-top:-5px;
+  	background-color:rgba(251,251,251,.4);
+	border-radius:30px 30px 30px 30px;
+	}
+	
+nav a {
+	margin-left:30px;
+	display: inline-block;
+    color:#000000;	
+	text-align: center;
+    padding: 10px 10px;
+    text-decoration: none;
+	}
+img{ margin-left:30px; margin-top:20px;}
+
+nav a:hover
+{    border-radius:30px 30px 30px 30px;
+	background-color:rgba(0,0,0,.2);
+	color:ffffff;
+
+}
+
+.end
+{ 
+    margin-top:100px;
+  	background-color:rgba(251,251,251,.4);
+	border-radius:10px 10px 10px 10px;	
+	padding:10px;
+}
+
+.IMG
+{  
+ -webkit-box-reflect:below -5px -webkit-gradient(linear, left top, left bottom, from(transparent), color-stop(50%,transparent) ,       to(rgba(250,250,250,.2)));
+  margin-top:60px;
+  margin-left:30px;
+
+    }
+	
+</style>
+<body background="background2.jpg">
+<a href="https://www.facebook.com/Love_quotes_1311-1815063365478273/"><img src="icon/if_facebook_313103.png" width="48" height="48" style="margin-top:-5px;float:right;"></a>
+<a href="https://www.instagram.com/love_quotes_1311"><img src="icon/if_JD-03_2246821.png" width="48" height="48" style="margin-right:-30;margin-top:-5px;float:right;" ></a>
+<div class="name">
+<img src="icon/if_We Love Icons_51882.png" class="IMG" >
+<h1>  Love_quotes_1311</h1>
+
+<br>
+<div>
+<nav>
+<a href="index.php" style="margin-left:90px;"><b>Home</b></a>
+<a href="Suggestion.php"><b>Suggestion</b></a>
+<a href="image.html" ><b>Images</b></a>
+<a href="quotes.html" ><b>Quotes</b></a>
+<div style="float:right; margin-right:30px;">
+<a href="https://www.instagram.com/love_quotes_1311"><b>Join Us</b></a>
+</div>
+</nav>
+</body>
+</html>
